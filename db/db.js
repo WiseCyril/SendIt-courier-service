b90c26dcff66bd3fdf977601d27467b95026b5ba
unknown <nnamadimuche@gmail.com>
2018-11-08 20:34:58 +0100

    create dummy database

--- db/db.js
+++ db/db.js
@@ -1,65 +1,66 @@
 //create dummy database
 
 const userData = {
-  user1: {
-    firstname: 'Chinyere',
-    lastname: 'Maggi',
-    email: 'chinyere.mag@gmail.com',
-    password: 'chinmag2018'
-  },
-  user3: {
-    firstname: 'Justice',
-    lastname: 'Freeman',
-    email: 'justfree@me.com',
-    password: 'paradion89'
-  },
-  user4: {
-    firstname: 'Tola',
-    lastname: 'Ricardo',
-    email: 'tolar@yahoo.com',
-    password: 'tolar@gmail.com'
-  },
-  user5: {
-    firstname: 'Grace',
-    lastname: 'Mercry',
-    email: 'gracey@hotmail.com',
-    password: 'grade1,90'
-  }
+    user1: {
+        firstname: 'Chinyere',
+        lastname: 'Maggi',
+        email: 'chinyere.mag@gmail.com',
+        password: 'chinmag2018'
+    },
+    user3: {
+        firstname: 'Justice',
+        lastname: 'Freeman',
+        email: 'justfree@me.com',
+        password: 'paradion89'
+    },
+    user4: {
+        firstname: 'Tola',
+        lastname: 'Ricardo',
+        email: 'tolar@yahoo.com',
+        password: 'tolar@gmail.com'
+    },
+    user5: {
+        firstname: 'Grace',
+        lastname: 'Mercry',
+        email: 'gracey@hotmail.com',
+        password: 'grade1,90'
+    }
 };
 
 
 const parcel = {
-	parcel1:
-	{
-		id: 1,
-    	weight: "3kg",
-    	pickup: "Sokoto street, Abia",
-    	receiver_name: "Kunle Abubakar",
-    	destination: "Ibadan street, Delta"
+    parcel1:
+    {
+        id: 1,
+        weight: "3kg",
+        pickup: "Sokoto street, Abia",
+        receiver_name: "Kunle Abubakar",
+        destination: "Ibadan street, Delta"
     },
+
     parcel2:
-	{
-		id: 2,
-    	weight: "4kg",
-    	pickup: "Niger street, Enugu",
-    	receiver_name: "Abu Taylor",
-    	destination: "Ibukun street, Osogbo"
+    {
+        id: 2,
+        weight: "4kg",
+        pickup: "Niger street, Enugu",
+        receiver_name: "Abu Taylor",
+        destination: "Ibukun street, Osogbo"
     },
     parcel3:
-	{
-		id: 3,
-    	weight: "4kg",
-    	pickup: "Ndu street, Rivers",
-    	receiver_name: "Kareem Jimmy",
-    	destination: "Indiana way, Lagos"
+    {
+        id: 3,
+        weight: "4kg",
+        pickup: "Ndu street, Rivers",
+        receiver_name: "Kareem Jimmy",
+        destination: "Indiana way, Lagos"
     },
     parcel4:
-	{
-		id: 4,
-    	weight: "5kg",
-    	pickup: "Maritama, Abuja",
-    	receiver_name: "Clifford Samuel",
-    	destination: "Ekiti estate, Edo"
+    {
+        id: 4,
+        weight: "5kg",
+        pickup: "Maritama, Abuja",
+        receiver_name: "Clifford Samuel",
+        destination: "Ekiti estate, Edo"
     }
 };
 
