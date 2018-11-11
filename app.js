43e7f3380bf000fedc71ae8bd11013bb953f2dac
unknown <nnamadimuche@gmail.com>
2018-11-11 19:31:26 +0100

    modify app.js port

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import router from './routes/parcel.js';
+import express from 'express'
+import bodyParser from 'body-parser'
+import router from './routes/parcel.js'
 
 // import body-parser into app.js
 
 // Set up the express app
-const app = express();
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Welcome to sendit courier service')
+})
 
 // Parse incoming requests data
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(router);
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(router)
 
-const PORT = 3000;
+app.all('*', (req, res) => {
+  res.send('Route not available at the moment')
+})
+
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-  console.log(`server running on port ${PORT}`);
-});
+  console.log(`server running on port ${PORT}`)
+})
