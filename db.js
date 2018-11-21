2953ed5b56ca5a09d6bd38f9965d1a050c9bc93d
unknown <nnamadimuche@gmail.com>
2018-11-21 09:33:02 +0100

    database setup

--- db.js
+++ db.js
@@ -21,13 +21,14 @@ pool.on('connect', () => {
 const createParcelTables = () => {
   const queryText = `CREATE TABLE IF NOT EXISTS
       parcel(
-        id UUID PRIMARY,
-        weigh VARCHAR(255) NOT NULL,
+        id UUID PRIMARY KEY,
+        parcel_id INTEGER NOT NULL,
+        weight INTEGER NOT NULL,
         receiver_name VARCHAR(255) NOT NULL,
-        pickup VARCHAR(255) NOT NULL
+        pickup VARCHAR(255) NOT NULL,
         destination VARCHAR(255) NOT NULL,
         created_order_date TIMESTAMP,
-        FOREIGN KEY (id) REFERENCES users (id) ON DELETE CASCADE
+        FOREIGN KEY (id) REFERENCES users (parcel_id) ON DELETE CASCADE
         )`;
 
   pool.query(queryText)
